de6262ebb6e9db7b50b4f6b3bece79d4ec9e703b
tangpeng <tangpeng@cc-pg.cn>
2017-06-13 16:45:51 +0800

    ccsdk

---
+++ TPSpec1/Classes/CCSDK/DWSDK.h
@@ -0,0 +1,17 @@
+#import "DWLog.h"
+#import "DWMoviePlayerController.h"
+#import "DWUploader.h"
+#import "DWVideoCompressController.h"
+#import "DWAdInfo.h"
+
+#import "DWUtils.h"
+#import "DWPlayInfo.h"
+#import "DWDrmServer.h"
+//下载相关
+#import "DWDownloadSessionManager.h"
+#import "DWDownloadModel.h"
+#import "NSURLSession+DWCorrectedResumeData.h"
+#import "DWDownloadUtility.h"
+
+
+#define DWSDK_VERSION @"ios_2.8.5"
